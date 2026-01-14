f03f7a1156dbee3e37abfeedd7433fd24fbea8b6
IBookki <jubloutheo0@gmail.com>
2026-01-14 09:33:57 +0100

    Add images and title page

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const albums = document.querySelectorAll('.album');
+const albums = document.querySelectorAll(".album");
+const mainPage = document.getElementById("main-page");
+const scene = document.getElementById("scene");
 let scrollY = 0;
 let currentY = 0;
+const maxScroll = albums.length * 1000
 
 function animate() {
   currentY += (scrollY - currentY) * 0.08;
@@ -11,11 +14,21 @@ function animate() {
     album.style.transform = `translate3d(${x}px, 0, ${z}px)`;
     album.style.opacity = z > 200 || z < -3000 ? 0 : 1;
   });
+
+  if (currentY >= maxScroll - 500) {
+    mainPage.classList.add("visible");
+    scene.style.opacity = "0";
+  } else {
+    mainPage.classList.remove("visible");
+    scene.style.opacity = "1";
+  }
+
   requestAnimationFrame(animate);
 }
-window.addEventListener('wheel', (e) => {
+
+window.addEventListener("wheel", (e) => {
   scrollY += e.deltaY * 2;
-  scrollY = Math.max(0, Math.min(scrollY, (albums.length - 1) * 1000));
+  scrollY = Math.max(0, Math.min(scrollY, maxScroll));
 });
 
 animate();
